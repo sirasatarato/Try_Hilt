7911a10f49ee4cdf88959bc1f777411fdb2ac1a7
sirasatarato <sirasatarato@gmail.com>
2020-09-14 20:12:27 +0900

    Create ViewModel

--- try_hilt/app/src/main/java/com/silso/try_hilt/MainActivity.kt
+++ try_hilt/app/src/main/java/com/silso/try_hilt/MainActivity.kt
@@ -2,8 +2,11 @@ package com.silso.try_hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ try_hilt/app/src/main/java/com/silso/try_hilt/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.silso.try_hilt
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class MainViewModel @ViewModelInject constructor(): ViewModel() {
+
+}
