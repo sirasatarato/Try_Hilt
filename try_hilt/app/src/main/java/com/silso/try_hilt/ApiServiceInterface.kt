20cfda8ddf1969fc29b3f89d8b019c1b065e8dfa
sirasatarato <sirasatarato@gmail.com>
2020-09-15 23:06:07 +0900

    Create UserData

--- try_hilt/app/src/main/java/com/silso/try_hilt/ApiServiceInterface.kt
+++ try_hilt/app/src/main/java/com/silso/try_hilt/ApiServiceInterface.kt
@@ -1,9 +1,10 @@
 package com.silso.try_hilt
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiServiceInterface {
     @GET("/posts/{id}")
-    fun getUserInfo(@Path("id") id: Int)
+    fun getUserInfo(@Path("id") id: Int): Call<UserData>
 }
---
+++ try_hilt/app/src/main/java/com/silso/try_hilt/UserData.kt
@@ -0,0 +1,8 @@
+package com.silso.try_hilt
+
+data class UserData(
+    var userId: Int = 0,
+    var id: Int = 0,
+    var title: String = "",
+    var body: String = ""
+)
