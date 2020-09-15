02f6f2845f9e1079c651001d1f08b859839202f5
sirasatarato <sirasatarato@gmail.com>
2020-09-15 23:03:50 +0900

    Create ApiServiceInterface

---
+++ try_hilt/app/src/main/java/com/silso/try_hilt/ApiServiceInterface.kt
@@ -0,0 +1,9 @@
+package com.silso.try_hilt
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiServiceInterface {
+    @GET("/posts/{id}")
+    fun getUserInfo(@Path("id") id: Int)
+}
