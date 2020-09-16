d27a11d5a9dae571c99ad909bb6ddbd1397f4fc2
sirasatarato <sirasatarato@gmail.com>
2020-09-16 22:21:30 +0900

    Add Coroutine in gradle

--- try_hilt/app/src/main/java/com/silso/try_hilt/ApiServiceInterface.kt
+++ try_hilt/app/src/main/java/com/silso/try_hilt/ApiServiceInterface.kt
@@ -1,10 +1,9 @@
 package com.silso.try_hilt
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiServiceInterface {
     @GET("/posts/{id}")
-    fun getUserInfo(@Path("id") id: Int): Call<UserData>
+    suspend fun getUserInfo(@Path("id") id: Int): UserData
 }
--- try_hilt/app/src/main/java/com/silso/try_hilt/MainViewModel.kt
+++ try_hilt/app/src/main/java/com/silso/try_hilt/MainViewModel.kt
@@ -2,7 +2,11 @@ package com.silso.try_hilt
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
 
-class MainViewModel @ViewModelInject constructor(): ViewModel() {
-
+class MainViewModel @ViewModelInject constructor(private val apiService: ApiServiceInterface): ViewModel() {
+    fun getUser(id: Int) = liveData(Dispatchers.IO) {
+        emit(apiService.getUserInfo(id))
+    }
 }
