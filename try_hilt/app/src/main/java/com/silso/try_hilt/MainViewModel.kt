c91a2f5c33d7b9d2db33eb0e0e9f22c0fc35c90a
sirasatarato <sirasatarato@gmail.com>
2020-09-17 11:25:01 +0900

    Add getUserFromServer

--- try_hilt/app/src/main/java/com/silso/try_hilt/MainViewModel.kt
+++ try_hilt/app/src/main/java/com/silso/try_hilt/MainViewModel.kt
@@ -1,10 +1,34 @@
 package com.silso.try_hilt
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(private val apiService: ApiServiceInterface) :
     ViewModel() {
+
+    val post: MutableLiveData<UserData> by lazy {
+        MutableLiveData<UserData>()
+    }
+
+    val errorMessage: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    fun getUserFromServer(id: Int) {
+        viewModelScope.launch {
+            val retrofitPost = getUserRequest(id)
+            when (retrofitPost) {
+                is Result.Success -> { post.postValue(retrofitPost.data)}
+                is Result.Error -> {
+                    errorMessage.postValue(retrofitPost.exception)
+                }
+            }
+        }
+
+    }
 
     suspend fun getUserRequest(id: Int): Result<UserData> {
         return safeApiCall(call = { apiService.getUserInfo(id) })
